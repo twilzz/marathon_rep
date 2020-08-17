c1f347c2cd1f232894c79f01f2ca3445bb363c30
Alexei aka Twils <twils@ya.ru>
2020-08-17 16:14:35 +0300

    Украшательства

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 setTimeout(() => {
     console.log("Js подключился");
 }, 5000);
+
+const firstDiv = document.querySelector('.welcome');
+console.log(firstDiv);
+
+firstDiv.addEventListener('click', () => {
+    firstDiv.innerHTML = "<p>JS работает</p><img src='img/yes.png'>";
+})
