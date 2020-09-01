51e2b954b5f0c2c1eec206af5aea5157ebe32652
Alexey aka Twils <twils@ya.ru>
2020-09-01 22:42:29 +0300

    перерефакторился на Классы

--- js/main.js
+++ js/main.js
@@ -1,66 +1,95 @@
-const firstTask = document.querySelector('#task_1'),
-      secondTask = document.querySelector('#task_2')
-      firstDiv = document.querySelector('.welcome')
-    let sent_1 = '', 
-        sent_2='',
-        counter_1 = 0,
-        counter_2 = 0;
+import { Pokemon } from './pokemon.js';
+import { randomizeDmg } from './randomize.js';
 
-firstTask.addEventListener('click', () => {
-    sent_1 = prompt('Первое предложение', 'Мама мыла раму'),
-    sent_2 = prompt('Второе предложение', 'Папа ржал как конь');   
-    getRow(sent_1,sent_2); 
-    let sentDiv = document.createElement('p');
-    sentDiv.innerHTML = `<p>${sent_1} - ${counter_1}</p>
-                          <p>${sent_2} - ${counter_2}</p>`
-    firstDiv.append(sentDiv);
-    let winnerDiv = document.createElement('div');
-if (counter_1 > counter_2) {
-    winnerDiv.innerHTML = `<div>${sent_1}</div>`
-} else if (counter_2 > counter_1) {
-    winnerDiv.innerHTML = `<div>${sent_2}</div>`
-} else if (counter_1 !== 0 && counter_2 !== 0 && counter_1 === counter_2) {
-    winnerDiv.innerHTML = `<div>В предложениях равное число букв "а"</div>`
-}
-firstDiv.append(winnerDiv);
+const player1 = new Pokemon ({
+    name: "Pikachu",
+    hp: 100,
+    type: 'electric',
+    selector: `character`
 });
-function getRow(a, b){
-    for (let i = 0; i < a.length; i++) {
-        if (a.charAt(i) === 'а') {
-            counter_1 ++
-            console.log('ALARM! Sent 1');
-        }
-    }
-    for (let i = 0; i < b.length; i++) {
-        if (b.charAt(i) === 'а') {
-            counter_2 ++
-            console.log('ALARM! Sent 2');
-        }
-    }
-    return counter_1, counter_2;
-};
 
-let phoneNum = '';
-secondTask.addEventListener('click', ()=> {
- phoneNum = prompt("Введите номер телефона", '+71234567890')
-  let winnerDiv = document.createElement('div');
-    winnerDiv.innerHTML = `<div>${formatPhone(phoneNum)}</div>`
-    firstDiv.append(winnerDiv);
+const player2 = new Pokemon ({
+    name: "Charmander",
+    hp: 100,
+    type: 'fire',
+    selector: `enemy`
 });
-function formatPhone(tel) {
-    console.log(tel.length);
-    let newNum = ''
-    for (let i = 0; i <tel.length; i++) {
-        switch(i) {
-            case 2: newNum = newNum + " (" + tel[i];
-            break;
-            case 5: newNum = newNum + ") " + tel[i];
-            break;
-            case 8:
-            case 10: newNum = newNum + "-" + tel[i];
-            break;
-            default: newNum += tel[i];
-        }
-    }
-    return newNum;
+
+
+
+
+const btnFight= document.querySelector('#btn-kick'), 
+      btnParent = document.querySelector('.control'),
+      fightLog = document.querySelector('#logs'),
+      paragrathLog = document.createElement('p'),
+      spellChar = document.querySelector('.spell_char'),
+      spellEnemy = document.querySelector('.spell_enemy');
+
+let countChar = clickCounter(6);
+let countEnem = clickCounter(6);
+
+
+spellChar.addEventListener('click', () => {
+    let a = countChar();
+    if (a === 0) {
+        spellChar.innerText = 0;
+        spellChar.disabled = true;
+        paragrathLog.innerText = `${player1.name} исчерпал свои удары`;
+        fightLog.append(paragrathLog);
+        } else {
+        player2.kickAss(randomizeDmg(20), (dmg) =>{
+            paragrathLog.innerText = generateLog(player1,player2,dmg);
+            fightLog.append(paragrathLog);
+        });
+        spellChar.innerText = a;
+           }
+});
+
+spellEnemy.addEventListener('click', () => {
+    let b = countEnem();
+    if (b === 0) {
+        spellEnemy.innerText = 0;
+        spellEnemy.disabled = true;
+        paragrathLog.innerText = `${player2.name} исчерпал свои удары`;
+        fightLog.append(paragrathLog);
+        } else {
+        player1.kickAss(randomizeDmg(20), (dmg) =>{
+            paragrathLog.innerText = generateLog(player2,player1,dmg);
+            fightLog.append(paragrathLog);
+        });
+        spellEnemy.innerText = b;
+           }
+});
+function clickCounter (b) {
+    let local = b;
+    return ()=>  {
+        if (local === 0) {
+        return local;
+    } else {
+        return --local;}
+}}
+
+
+function generateLog(firstPerson, secondPerson, dmg) {
+    const logs = [
+        `${secondPerson.name} вспомнил что-то важное, но неожиданно ${firstPerson.name}, ударил в предплечье врага.на ${dmg} урона, оставив [${secondPerson.hp.current}|${secondPerson.hp.total}]`,
+        `${secondPerson.name} поперхнулся, и за это ${firstPerson.name} приложил прямой удар коленом в пах на ${dmg} урона, оставив [${secondPerson.hp.current}|${secondPerson.hp.total}]`,
+        `${secondPerson.name} забылся, но в это время ${firstPerson.name},неслышно подойдя сзади, ударил на ${dmg} урона, оставив [${secondPerson.hp.current}|${secondPerson.hp.total}]`,
+        `${secondPerson.name} пришел в себя, но неожиданно ${firstPerson.name} нанес мощнейший удар на ${dmg} урона, оставив [${secondPerson.hp.current}|${secondPerson.hp.total}]`,
+        `${secondPerson.name} поперхнулся, но в это время ${firstPerson.name} раздробил кулаком \<вырезанно цензурой\> противника на ${dmg} урона, оставив [${secondPerson.hp.current}|${secondPerson.hp.total}]`,
+        `${secondPerson.name} удивился, а ${firstPerson.name} пошатнувшись влепил подлый удар на ${dmg} урона, оставив [${secondPerson.hp.current}|${secondPerson.hp.total}]`,
+        `${secondPerson.name} высморкался, но неожиданно ${firstPerson.name} провел дробящий удар на ${dmg} урона, оставив [${secondPerson.hp.current}|${secondPerson.hp.total}]`,
+        `${secondPerson.name} пошатнулся, и  ${firstPerson.name} беспричинно ударил в ногу противника на ${dmg} урона, оставив [${secondPerson.hp.current}|${secondPerson.hp.total}]`,
+        `${secondPerson.name} расстроился, как вдруг, неожиданно ${firstPerson.name} влепил  в живот соперника на ${dmg} урона, оставив [${secondPerson.hp.current}|${secondPerson.hp.total}]`,
+        `${secondPerson.name} пытался что-то сказать, но ${firstPerson.name} разбил бровь сопернику на ${dmg} урона, оставив [${secondPerson.hp.current}|${secondPerson.hp.total}]`
+    ];
+    return logs[randomizeDmg(logs.length)-1];
 }
+btnFight.addEventListener('click', ()=>{
+    btnFight.innerText = result();
+    character.kickAss(randomizeDmg(20));
+    enemy.kickAss(randomizeDmg(20));
+});
+
+
+
